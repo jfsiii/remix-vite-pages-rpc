5776c936dce377d6adda7b93d04ac9a0b3636968
John Schulz <john.schulz@laerdal.com>
2024-06-17 19:55:24 -0400

    Add workers/services & access them in context
    To start:
    1) in a new terminal for each workers/* script
     - cd workers/hello-worker-a (or whatever)
     - npx wrangler dev
    2) (for dev) in a new terminal: npm run dev
    2) (for prod) in a new terminal: npm run build && npm run start

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
+import { json, type LoaderFunctionArgs, type MetaFunction } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
+
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,7 +12,24 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const loader = async ({ context }: LoaderFunctionArgs) => {
+  const { MY_JSON_VAR, MY_RPC, MY_SERVICE_A, MY_SERVICE_B,  MY_VAR, MY_VAR_A, } = context.cloudflare.env;
+  console.log({ MY_JSON_VAR, MY_RPC, MY_SERVICE_A, MY_SERVICE_B,  MY_VAR, MY_VAR_A, })
+  return json({
+    extra: context.extra,
+    env: {
+      MY_JSON_VAR,
+      MY_VAR,
+      MY_VAR_A,
+      MY_RPC: 'Cannot be serialized to JSON, but it is available in the context. See console.logs for more info.',
+      MY_SERVICE_A: 'Cannot be serialized to JSON, but it is available in the context. See console.logs for more info.',
+      MY_SERVICE_B: 'Cannot be serialized to JSON, but it is available in the context. See console.logs for more info.',
+    },
+  });
+};
+
 export default function Index() {
+  const {env, extra} = useLoaderData<typeof loader>()
   return (
     <div className="font-sans p-4">
       <h1 className="text-3xl">Welcome to Remix on Cloudflare</h1>
@@ -36,6 +55,14 @@ export default function Index() {
           </a>
         </li>
       </ul>
+      <h2 className="text-2xl mt-4">Cloudflare Environment Variables</h2>
+      <pre className="mt-4 p-4 bg-gray-100 rounded-md">
+        {JSON.stringify(env, null, 2)}
+      </pre>
+      <h2 className="text-2xl mt-4">Other Remix Context</h2>
+      <pre className="mt-4 p-4 bg-gray-100 rounded-md">
+        {JSON.stringify({extra}, null, 2)}
+      </pre>
     </div>
   );
 }
