b4099227197eb38d900db91af5e9b05f91ac7321
John Schulz <john.schulz@laerdal.com>
2024-06-17 20:12:52 -0400

    Can use RPC methods in `remix vite:dev` with `const` but *not* `using`

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import { json, type LoaderFunctionArgs, type MetaFunction } from "@remix-run/cloudflare";
 import { useLoaderData } from "@remix-run/react";
+import { M } from "node_modules/vite/dist/node/types.d-aGj9QkWt";
 
 
 export const meta: MetaFunction = () => {
@@ -15,6 +16,13 @@ export const meta: MetaFunction = () => {
 export const loader = async ({ context }: LoaderFunctionArgs) => {
   const { MY_JSON_VAR, MY_RPC, MY_SERVICE_A, MY_SERVICE_B,  MY_VAR, MY_VAR_A, } = context.cloudflare.env;
   console.log({ MY_JSON_VAR, MY_RPC, MY_SERVICE_A, MY_SERVICE_B,  MY_VAR, MY_VAR_A, })
+
+  // see https://github.com/cloudflare/workers-sdk/blob/5d3d12f75769f022629ace3aa402941889551d5a/fixtures/get-platform-proxy/tests/get-platform-proxy.env.test.ts#L199-L205
+  const counter = await MY_RPC.getCounter();
+  console.log(await counter.value); // 0
+  console.log(await counter.increment(4)); // 4
+  console.log(await counter.increment(8)); // 12
+  console.log(await counter.value); // 12
   return json({
     extra: context.extra,
     env: {
@@ -25,11 +33,12 @@ export const loader = async ({ context }: LoaderFunctionArgs) => {
       MY_SERVICE_A: 'Cannot be serialized to JSON, but it is available in the context. See console.logs for more info.',
       MY_SERVICE_B: 'Cannot be serialized to JSON, but it is available in the context. See console.logs for more info.',
     },
+    lastCounter: await counter.value,
   });
 };
 
 export default function Index() {
-  const {env, extra} = useLoaderData<typeof loader>()
+  const {env, extra, lastCounter} = useLoaderData<typeof loader>()
   return (
     <div className="font-sans p-4">
       <h1 className="text-3xl">Welcome to Remix on Cloudflare</h1>
@@ -62,6 +71,10 @@ export default function Index() {
       <h2 className="text-2xl mt-4">Other Remix Context</h2>
       <pre className="mt-4 p-4 bg-gray-100 rounded-md">
         {JSON.stringify({extra}, null, 2)}
+      </pre>
+      <h2 className="text-2xl mt-4">Result(s) of calling MY_RPC methods</h2>
+      <pre className="mt-4 p-4 bg-gray-100 rounded-md">
+        {JSON.stringify({lastCounter}, null, 2)}
       </pre>
     </div>
   );
--- load-context.ts
+++ load-context.ts
@@ -1,12 +1,20 @@
 import { type AppLoadContext } from '@remix-run/cloudflare';
+import { M } from 'node_modules/vite/dist/node/types.d-aGj9QkWt';
 import { type PlatformProxy } from "wrangler";
-
+import { NamedEntrypoint as RpcWorker  } from "./workers/rpc-worker/index";
 // When using `wrangler.toml` to configure bindings,
 // `wrangler types` will generate types for those bindings
 // into the global `Env` interface.
 // Env is from ./worker-configuration.d.ts (created by `wrangler types` or `npm run typegen`)
 
-type Cloudflare = Omit<PlatformProxy<Env>, "dispose">;
+
+// If left as Env, the RPC worker only shows fetch & connect methods; not those it actually has
+interface Env2 extends Env {
+  MY_RPC: Service<RpcWorker>;
+}
+
+type Cloudflare = Omit<PlatformProxy<Env2>, "dispose">;
+
 
 declare module '@remix-run/cloudflare' {
   interface AppLoadContext {
