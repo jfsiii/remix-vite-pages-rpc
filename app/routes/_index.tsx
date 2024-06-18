a9f0ebb40030a60b6af47dfc36c9a1e7a66efd5f
John Schulz <john.schulz@laerdal.com>
2024-06-17 20:58:29 -0400

    Call a service directly (via its .fetch())

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,5 @@
 import { json, type LoaderFunctionArgs, type MetaFunction } from "@remix-run/cloudflare";
 import { useLoaderData } from "@remix-run/react";
-import { M } from "node_modules/vite/dist/node/types.d-aGj9QkWt";
 
 
 export const meta: MetaFunction = () => {
@@ -16,6 +15,7 @@ export const meta: MetaFunction = () => {
 export const loader = async ({ context }: LoaderFunctionArgs) => {
   const { MY_JSON_VAR, MY_RPC, MY_SERVICE_A, MY_SERVICE_B,  MY_VAR, MY_VAR_A, } = context.cloudflare.env;
   console.log({ MY_JSON_VAR, MY_RPC, MY_SERVICE_A, MY_SERVICE_B,  MY_VAR, MY_VAR_A, })
+  const resultA = await MY_SERVICE_A.fetch("http://0.0.0.0");
 
   // see https://github.com/cloudflare/workers-sdk/blob/5d3d12f75769f022629ace3aa402941889551d5a/fixtures/get-platform-proxy/tests/get-platform-proxy.env.test.ts#L199-L205
   const counter = await MY_RPC.getCounter();
@@ -34,11 +34,12 @@ export const loader = async ({ context }: LoaderFunctionArgs) => {
       MY_SERVICE_B: 'Cannot be serialized to JSON, but it is available in the context. See console.logs for more info.',
     },
     lastCounter: await counter.value,
+    serviceA: await resultA.text(),
   });
 };
 
 export default function Index() {
-  const {env, extra, lastCounter} = useLoaderData<typeof loader>()
+  const {env, extra, lastCounter, serviceA} = useLoaderData<typeof loader>()
   return (
     <div className="font-sans p-4">
       <h1 className="text-3xl">Welcome to Remix on Cloudflare</h1>
@@ -72,9 +73,12 @@ export default function Index() {
       <pre className="mt-4 p-4 bg-gray-100 rounded-md">
         {JSON.stringify({extra}, null, 2)}
       </pre>
-      <h2 className="text-2xl mt-4">Result(s) of calling MY_RPC methods</h2>
+      <h2 className="text-2xl mt-4">Result(s) of calling Service/RPC methods</h2>
       <pre className="mt-4 p-4 bg-gray-100 rounded-md">
-        {JSON.stringify({lastCounter}, null, 2)}
+        {JSON.stringify({
+          serviceA,
+          rpcLastCounter: lastCounter,
+          }, null, 2)}
       </pre>
     </div>
   );
